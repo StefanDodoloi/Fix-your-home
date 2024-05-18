c3e193950cfc5b2f6339f2cba9b4d3718051edd5
StefanDodoloi <153545504+StefanDodoloi@users.noreply.github.com>
2024-05-18 15:09:33 +0300

    Add files via upload

--- adwebsite/urls.py
+++ adwebsite/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile_update/', user_views.profile_update, name='profile_update'),
-    path('profile/', user_views.profile, name='profile'),
+    path('profile/<str:username>/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path("logout/", user_views.logout_view, name="logout"),
     path('', include('ads.urls')),
